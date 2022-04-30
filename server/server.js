ffc370cf7eb658a3415f51b4b07d74b4ac87b52d
Bislan Takakho <b.takakho@gmail.com>
2022-04-30 18:07:44 +0300

    Try socket.io

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const { Server } = require('socket.io')
+
+const port = process.env.PORT || 4001
+
+const app = express()
+
+const allowedOrigins = ['http://localhost:3000']
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+)
+
+app.use('/', (req, res) => {
+  return res.json({ hello: 'world' })
+})
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  path: '/signalling-server/',
+  cors: {
+    origin: allowedOrigins,
+  },
+})
+
+let interval
+
+io.on('connection', (socket) => {
+  console.log('New client connected', socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+    clearInterval(interval)
+  })
+})
+
+server.listen(port, () => console.log(`Listening on http://localhost:${port}`))
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,24 @@
-export const App = () => {
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+const ENDPOINT = "http://localhost:4001";
+
+function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = io(ENDPOINT, { path: '/signalling-server/' });
+
+    socket.on("connect", () => {
+      console.log('connected', socket.id);
+      setResponse(socket.id)
+    });
+  }, []);
+
   return (
-    <div>app</div>
-  )
+    <div>
+      socketID: {response}
+    </div>
+  );
 }
+
+export { App }
